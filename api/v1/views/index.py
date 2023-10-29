496a64bf5f15fcf9ba3862149eee0b36883dfbd5
Adane <adanemoges6@gmail.com>
2023-10-30 02:53:19 +0300

    task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the State class."""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status')
+def status():
+    """Return the status of the API."""
+    return jsonify({"status": "OK"})
