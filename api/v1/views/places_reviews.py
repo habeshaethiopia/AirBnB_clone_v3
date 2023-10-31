29bc170f6141beff32822bfbaaa4d2eb9b89da50
Adane <adanemoges6@gmail.com>
2023-11-01 01:17:52 +0300

    pycode fixed

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -10,11 +10,11 @@ from models.review import Review
 from models.place import Place
 
 
-@app_views.route("/place/<place_id>/review", strict_slashes=False,
-                 methods=["GET"])
+@app_views.route("/place/<place_id>/review",
+                 strict_slashes=False, methods=["GET"])
 def get_reviews(place_id):
     """
-        GET Request for reviews on a place
+    GET Request for reviews on a place
     """
     return parent_model(Place, place_id, "reviews")
 
@@ -23,35 +23,35 @@ def get_reviews(place_id):
                  strict_slashes=False, methods=["GET"])
 def get_review(review_id):
     """
-        GET Request for a review
+    GET Request for a review
     """
     return get_model(Review, review_id)
 
 
 @app_views.route("/review/<review_id>",
-                 methods=["DELETE"] ,strict_slashes=False)
+                 methods=["DELETE"], strict_slashes=False)
 def delete_review(review_id):
     """
-        DELETE Request for a review
+    DELETE Request for a review
     """
     return delete(Review, review_id)
 
 
 @app_views.route("/place/<place_id>/review",
-                 strict_slashes=False,
-                 methods=["POST"])
+                 strict_slashes=False, methods=["POST"])
 def post_review(place_id):
     """
-        POST Request for a review
+    POST Request for a review
     """
     return post(Review, Place, place_id, {"text", "user_id"})
 
 
-@app_views.route("/review/<review_id>",
-                 methods=["PUT"], strict_slashes=False)
+@app_views.route("/review/<review_id>", methods=["PUT"], strict_slashes=False)
 def put_review(review_id):
     """
-        PUT Request for a review
+    PUT Request for a review
     """
-    return put(Review, review_id, ["id", "created_at", "updated_at", "user_id",
-               "place_id"])
+    return put(
+        Review, review_id,
+        ["id", "created_at", "updated_at", "user_id", "place_id"]
+    )
