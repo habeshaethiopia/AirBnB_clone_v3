d1788131d1259c5d16711debdfdd20dd5b7dfe25
Adane <adanemoges6@gmail.com>
2023-10-31 04:28:35 +0300

    try this one

---
+++ api/v1/views/cities.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+view for cities
+"""
+
+from api.v1.views import *
+from flask import Flask, jsonify
+from models import storage
+from models.city import City
+from models.state import State
+from flasgger import swag_from
+
+
+@app_views.route("/states/<state_id>/cities", strict_slashes=False, methods=["GET"])
+@swag_from("../doc/cities/get.yml")
+def get_cities(state_id):
+    """
+    GET Request all cities in a State
+    """
+    all_cities = storage.get(City, state_id)
+    if all_cities:
+        return jsonify([p.to_dict() for p in getattr(City, "cities")]), 200
+    abort(404)
+
+
+
+@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["GET"])
+@swag_from("../doc/cities/getById.yml")
+def get_city(city_id):
+    """
+    GET Request for a city
+    """
+    return get_model(City, city_id)
+
+
+@app_views.route("/cities/<city_id>", methods=["DELETE"])
+@swag_from("../doc/cities/delete.yml")
+def delete_city(city_id):
+    """
+    DELETE Request for a city
+    """
+    return delete(City, city_id)
+
+
+@app_views.route("/states/<state_id>/cities", strict_slashes=False, methods=["POST"])
+@swag_from("../doc/cities/post.yml")
+def post_city(state_id):
+    """
+    POST Request for a city
+    """
+    return post(City, State, state_id, {"name"})
+
+
+@app_views.route("/cities/<city_id>", methods=["PUT"])
+@swag_from("../doc/cities/put.yml")
+def put_city(city_id):
+    """
+    PUT Request for a city
+    """
+    return put(City, city_id, ["id", "created_at", "updated_at"])
