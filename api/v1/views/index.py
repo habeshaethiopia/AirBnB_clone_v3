87fd4c8e5436ed7748acf0ee0e2218f8e955cf28
Adane <adanemoges6@gmail.com>
2023-10-31 23:13:13 +0300

    noting is working for index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 """Defines the index"""
-from api.v1.views import app_views
-from flask import jsonify
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from api.v1.views import app_views, Amenity, City, Place, Review, State, User
+from flask import Flask, jsonify
 
 
 @app_views.route("/status", strict_slashes=False, methods=["GET"])
@@ -16,19 +10,15 @@ def status():
     """Return the status of the API."""
     return jsonify({"status": "OK"}), 200
 
-
-@app_views.route("/stats", strict_slashes=False, methods=["GET"])
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
 def stats():
     """Return the stats of the API."""
-    classes = {
-        "states": State,
-        "users": User,
-        "amenities": Amenity,
-        "cities": City,
-        "places": Place,
-        "reviews": Review,
+    counts = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
     }
-    for key in classes:
-        classes[key] = storage.count(classes[key])
-
-    return jsonify(classes), 200
+    return jsonify(counts), 200
